2dbe181714ace2e943dc502ddb5e24b948be83e9
Matt Bray <mattjbray@gmail.com>
2015-06-23 17:33:45 +0100

    hlint

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -18,61 +18,61 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "Problem 1" $ do
-    it "solves the given example" $ do
+  describe "Problem 1" $
+    it "solves the given example" $
       sumMultiples 3 5 9 `shouldBe` 23
 
   describe "Problem 2" $ do
-    it "generates Fibonnaci numbers" $ do
+    it "generates Fibonnaci numbers" $
       -- we start at 0
       take 12 fibs `shouldBe` [0, 1 ,1 , 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    it "sums even fibs" $ do
+    it "sums even fibs" $
       sumEvenFibsTo 90 `shouldBe` 2 + 8 + 34
 
-  describe "Problem 3" $ do
-    it "solves the given example" $ do
+  describe "Problem 3" $
+    it "solves the given example" $
       largestPrimeFactor 13195 `shouldBe` 29
 
-  describe "Problem 4" $ do
-    it "solves the given example" $ do
+  describe "Problem 4" $
+    it "solves the given example" $
       largestPalindromeProduct 10 99 `shouldBe` 9009
 
-  describe "Problem 5" $ do
-    it "solves the given example" $ do
+  describe "Problem 5" $
+    it "solves the given example" $
       smallestEvenlyDivisibleByAllTo 10 `shouldBe` 2520
 
-  describe "Problem 6" $ do
-    it "solves the given example" $ do
+  describe "Problem 6" $
+    it "solves the given example" $
       diffSquares [1..10] `shouldBe` 2640
 
-  describe "Problem 7" $ do
-    it "solves the given example" $ do
+  describe "Problem 7" $
+    it "solves the given example" $
       primes !! 5 `shouldBe` 13
 
-  describe "Problem 8" $ do
-    it "solves the given example" $ do
+  describe "Problem 8" $
+    it "solves the given example" $
       findLargestProductOfLength 4 `shouldBe` 5832
 
-  describe "Problem 9" $ do
-    it "solves an example" $ do
+  describe "Problem 9" $
+    it "solves an example" $
       head (productsWhereTrianglesSumTo 12) `shouldBe` 3 * 4 * 5
 
-  describe "Problem 10" $ do
-    it "solves the given example" $ do
+  describe "Problem 10" $
+    it "solves the given example" $
       sumPrimesTo 10 `shouldBe` 17
 
-  describe "Problem 11" $ do
-    it "finds the largest product of four numbers at a given position" $ do
+  describe "Problem 11" $
+    it "finds the largest product of four numbers at a given position" $
       largestAt 3 0 `shouldBe` maximum [97 * 38 * 15 * 0, 97 * 40 * 73 * 23, 19 * 17 * 79 * 11, 97 * 99 * 49 * 52]
 
-  describe "Problem 12" $ do
-    it "solves the given example" $ do
+  describe "Problem 12" $
+    it "solves the given example" $
       firstTriangleWithNDivisors 5 `shouldBe` 28
 
-  describe "Problem 14" $ do
-    it "solves the given example" $ do
+  describe "Problem 14" $
+    it "solves the given example" $
       collatz 13 `shouldBe` [13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
 
-  describe "Problem 15" $ do
-    it "solves the given example" $ do
+  describe "Problem 15" $
+    it "solves the given example" $
       routes 2 `shouldBe` 6
